90b22e0dc995f0edbc3dbdaa36809847333b59dd
ovvn <omazinov@gmail.com>
2014-08-14 17:48:46 +0400

    Initial commit.

---
+++ helper.js
@@ -0,0 +1,34 @@
+var createCanvas, d3, drawHorizontalAxis, drawVerticalAxis, toFixed2;
+
+d3 = require('d3');
+
+createCanvas = function(el, width, height, margin, callFunc) {
+  var result;
+  result = d3.select(el).append("svg:svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("svg:g").attr("transform", "translate(" + margin.left + "," + margin.top + ")").style("font", "10px sans-serif");
+  if (callFunc) {
+    result = result.call(callFunc);
+  }
+  return result;
+};
+
+drawVerticalAxis = function(canvas, component, title, id) {
+  if (id == null) {
+    id = "";
+  }
+  return canvas.append("g").attr("id", id).attr("class", "_y _axis axisLeft").call(component).append("text").attr("y", -12).attr("dy", ".71em").attr("x", 8).style("text-anchor", "inherit").text(title);
+};
+
+drawHorizontalAxis = function(canvas, component, width, height, title, id) {
+  return canvas.append("g").attr("id", id != null ? id : "").attr("class", "_x _axis").attr("transform", "translate(0," + height + ")").call(component).append("text").attr("class", "_x _title").attr("x", width).attr("y", 15).attr("dy", "0.5em").style("text-anchor", "start").text(title);
+};
+
+toFixed2 = function(x) {
+  return ~~(x * 100) / 100;
+};
+
+module.exports = {
+  createCanvas: createCanvas,
+  drawHorizontalAxis: drawHorizontalAxis,
+  drawVerticalAxis: drawVerticalAxis,
+  toFixed2: toFixed2
+};
---
+++ index.js
@@ -0,0 +1,176 @@
+var d3 = require('d3');
+var helper = require('./helper.js');
+var testData = require('./data.json');
+
+module.exports = BarChart;
+function BarChart() {}
+BarChart.prototype.view = __dirname;
+
+BarChart.prototype.init = function() {
+  var model = this.model;
+  model.setNull("data", []);
+  model.setNull("width", 200);
+  model.setNull("height", 100);
+  model.setNull("colors", ['#4f81bd', '#c0504d']);
+  model.setNull("groupByKey", "");
+  model.setNull("keys", []);
+
+  this.yScale = d3.scale.linear()
+    .range([0, model.get("height")]);
+  this.xScale = d3.scale.ordinal()
+    .rangeBands([0, model.get("width")], 0.1);
+
+  // scales: ranges
+  this.x0 = d3.scale.ordinal().rangeRoundBands([0, model.get("width")], .1);
+  this.x1 = d3.scale.ordinal();
+  this.y = d3.scale.linear().range([model.get("height"), 0]);
+  // color func
+  this.color = d3.scale.ordinal().range(model.get("colors"));
+  // axis
+  this.xAxis = d3.svg.axis().scale(this.x0).orient("bottom");
+  this.yAxis = d3.svg.axis().scale(this.y).orient("left");
+
+  this.setKeys();
+  this.transform();
+};
+
+BarChart.prototype.create = function() {
+  var model = this.model;
+  var that = this;
+
+  //that.transform();
+  that.draw();
+};
+
+BarChart.prototype.setKeys = function() {
+  var model = this.model;
+  var groupByKey = model.get("groupByKey") || "role";
+  var data = model.get("data");
+  if(!data[0])
+    data = testData;
+
+  var key;
+  this.keys = (function() {
+    var _results;
+    _results = [];
+    for (key in data[0]) {
+      if (key !== groupByKey) {
+        _results.push(key);
+      }
+    }
+    return _results;
+  }).call(this);
+};
+
+BarChart.prototype.transform = function() {
+  var model = this.model;
+  var that = this;
+  var data = model.get("data");
+  if(!data[0])
+    data = testData;
+  var height = model.get("height");
+  var groupByKey = model.get("groupByKey") || "role";
+  var maxVal, minVal;
+
+  // hack
+  if(this.keys.indexOf('properties') > -1)
+    this.keys.splice(this.keys.indexOf('properties'), 1)
+
+  // prepare data
+  data.forEach(function(d) {
+    d.properties = (that.keys).map(function(name) { return {name: name, value: +d[name]}; });
+  });
+
+  // Get Min and Max values
+  minVal = d3.min(data, function(d) {
+    return d3.min(d.properties, function(d) {
+      return d.value;
+    });
+  });
+
+  minVal = Math.min(0, minVal);
+
+  maxVal = d3.max(data, function(d) {
+    return d3.max(d.properties, function(d) {
+      return d.value;
+    });
+  });
+
+  // Get the x axis position (handle negative values)
+  this.xAxisTransform = height
+  if(minVal < 0 < maxVal)
+    this.xAxisTransform =  height * (maxVal / (maxVal - minVal))
+
+  this.xScale.domain(d3.range(data.length));
+  // this could be implemented as extent for a relative scale
+  this.yScale.domain([0, d3.max(data, function(d) { return d.value })]);
+
+  // scales: domains
+  this.x0.domain(data.map(function(d) {
+    return d[groupByKey];
+  }));
+  this.x1.domain(that.keys).rangeRoundBands([0, this.x0.rangeBand()]);
+  this.y.domain([minVal, maxVal]);
+
+  this.minVal = minVal;
+  this.maxVal = maxVal;
+};
+
+BarChart.prototype.draw = function() {
+  require('./d3.tip.min.js');
+
+  var that = this;
+  var model = this.model;
+  var data = model.get("data");
+  if (!data[0])
+    data = testData;
+  var groupByKey = model.get("groupByKey") || "role";
+
+  var height = model.get("height");
+  var barSel = d3.select(this.chart).selectAll("rect.bar")
+    .data(data)
+    .enter()
+    .append("g").attr("class", "g")
+    .attr("transform", function (d) {
+      return "translate(" + that.x0(d[groupByKey]) + ",0)";
+    });
+
+  // tip: move toFixed2 to BarChart.prototype.toFixed2?
+  var tip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([-10, 0])
+    .html(function (d) {
+      return "<strong>Value:</strong> <span style='color:red'>" + (helper.toFixed2(d.value)) + "</span>";
+    });
+
+  barSel.selectAll("rect")
+    .data(function (d) {
+      return d.properties;
+    })
+    .enter()
+    .append("rect")
+    .attr("width", that.x1.rangeBand())
+    .attr("x", function (d) {
+      return that.x1(d.name);
+    })
+    .attr("y", function (d) {
+      if (d.value < 0) {
+        return that.y(0);
+      } else {
+        return that.y(d.value);
+      }
+    })
+    .attr("height", function (d) {
+      if (d.value < 0) {
+        return that.y(d.value + that.maxVal);
+      } else {
+        return height - that.y(d.value + that.minVal);
+      }
+    })
+    .style("fill", function (d) {
+      return that.color(d.name);
+    })
+    .on("mouseover", tip.show)
+    .on("mouseout", tip.hide);
+
+};
